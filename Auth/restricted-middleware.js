19c3053fff301a57d846e1fa469d348a3bebd42f
unknown <jjamal95@gmail.com>
2019-09-01 16:48:22 -0500

    finished MVP

--- Auth/restricted-middleware.js
+++ Auth/restricted-middleware.js
@@ -1,3 +1,30 @@
+const bcrypt = require('bcrypt');
+const Users = require('./auth-model');
+
+
+
+
 module.exports = function restriction(req, res, next) {
+    const {username, password}  = req.headers;
+
+    if(username && password) {
+        Users.findBy({username})
+        .first()
+        .then(user => {
+          if(user && bcrypt.compareSync(password, user.password)) {
+            next();
+  
+          } else {
+              res.status(400).json({message: "Invalid Credentials"});
+          }  
+             
+        }) .catch(error => {
+            res.status(500).json(error)
+        });
+
+    } else {
+        res.status(400).json({message: "Please provide valid credentials"});
+    }
+
     res.status(401).json({message: "You shall not pass!" });
 };
